f2947ab64f1071eb6d2ec6cc1f7925fd2bd8c79c
Paul <soapmctravich@gmail.com>
2017-03-09 10:15:29 +0100

    Initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name thunamaxApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the thunamaxApp
+ */
+angular.module('thunamaxApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
