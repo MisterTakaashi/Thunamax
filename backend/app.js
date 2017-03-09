fd107e363f2303b92e4c61cfdfa5aa44c4f6927d
Paul <soapmctravich@gmail.com>
2017-03-09 16:17:31 +0100

    Ajout de la verification de la carte bancaire

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,41 @@
 var express = require('express');
+var bodyParser = require('body-parser')
+
+var cardChecker = require('./card.js');
+
 var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
+
+app.post('/checkout', function(req, res) {
+  let card = JSON.parse(req.body.card);
+
+  let cardResult = cardChecker.checkCard(card);
+  if (!cardResult.status){
+    res.json({status: false, error: cardResult.error});
+    return;
+  }
+
+  // Vérification du montant du paiement
+  let amount = req.body.amount;
+  if (amount == undefined){
+    res.json({status: false, error: "Le montant du paiement doit être renseigné"});
+    return;
+  }
+  if (!new RegExp(/^\d+$/).test(amount)){
+    res.json({status: false, error: "Le montant du paiement doit être un nombre"});
+    return;
+  }
+
+  res.json({status: true});
+})
 
 app.listen(4040, function () {
   console.log('Example app listening on port 4040!');
---
+++ backend/card.js
@@ -0,0 +1,30 @@
+module.exports.checkCard = function (card) {
+  // Vérification expiration
+  let expiration = card.expiration;
+  if (expiration == undefined){
+    return {status: false, error: "La date d'expiration doit être renseignée"};
+  }
+  if (!new RegExp(/^\d{2}\/\d{2}$/).test(expiration)){
+    return {status: false, error: "La date d'expiration est incorrecte"};
+  }
+
+  // Vérification numéro de carte
+  let number = card.number;
+  if (number == undefined){
+    return {status: false, error: "Le numéro de carte doit être renseigné"};
+  }
+  if (!new RegExp(/^\d{16}$/).test(number)){
+    return {status: false, error: "Le numéro de carte est incorrecte"};
+  }
+
+  // Vérification du CVV
+  let cvv = card.cvv;
+  if (cvv == undefined){
+    return {status: false, error: "Le crytpogramme de sécurité doit être renseigné"};
+  }
+  if (!new RegExp(/^\d{3}$/).test(cvv)){
+    return {status: false, error: "Le crytpogramme de sécurité est incorrect"};
+  }
+
+  return {status: true};
+}
